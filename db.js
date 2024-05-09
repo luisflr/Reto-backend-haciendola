9769e66b6caf2a9f3119498564abba5700290b46
luisflr <lfloresr@ulasalle.edu.pe>
2024-05-09 16:55:22 -0500

    [ADD] installed modules and register & login endpoints are created

---
+++ db.js
@@ -0,0 +1,6 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize('haciendola', 'luisflr', 'admin', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
---
+++ models/User.js
@@ -0,0 +1,21 @@
+import { Sequelize as sq, DataTypes as dt} from "sequelize";
+
+const UserModel = (sequelize) => {
+  const User = sequelize.define("user", {
+    username: {
+      type: dt.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: dt.STRING,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false
+  });
+  return User;
+}
+
+export default UserModel;
