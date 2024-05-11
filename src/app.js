abd29298dbe8d2adc2d8b1f77f0998343e98aed7
luisflr <lfloresr@ulasalle.edu.pe>
2024-05-11 18:41:46 -0500

    [ADD] adding cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import usersRoutes from './routes/user.route.js';
 import productsRoutes from './routes/products.route.js';
@@ -6,10 +7,11 @@ import productsRoutes from './routes/products.route.js';
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use(usersRoutes);
-app.use(productsRoutes)
+app.use(productsRoutes);
 
 export default app;
