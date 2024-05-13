d8271f18c55125eea721c0b4e1064cf2bbc6dbed
luisflr <lfloresr@ulasalle.edu.pe>
2024-05-13 17:27:37 -0500

    [UPD] udpate sequelize config

--- src/database/db.js
+++ src/database/db.js
@@ -3,12 +3,4 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME, 
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  }
-);
+export const sequelize = new Sequelize(process.env.DB_URL);
